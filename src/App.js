3fd5efc062040c4f48441bfc5671d9e8d1a8a2d7
vowval <jagannath.g@tringapps.com>
2022-11-18 12:43:44 +0530

    test 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload, OK.
+          Edit <code>src/App.js</code> and save to reload, OK, Yes.
         </p>
         <a
           className="App-link"
